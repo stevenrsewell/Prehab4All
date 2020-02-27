11d12b2ecbec0c1f5c327aef627ec6e18c0d5b4d
Bhavana <bhavana307@gmail.com>
2020-02-27 14:51:57 -0600

    added sign and sign-up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+// import { Router ,Switch ,Route } from "react-router-dom";
+import Login from "./components/Login"
+
+import Signup from "./components/Signup"
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Login  login={Login}/>
+   <Signup  Signup={Signup}/>
+  </div>
   );
 }
 
