0b2d26bdfd8b03c26ac0db063a452c3e0be45aa6
Steven Sewell <stevenrsewell@outlook.com>
2020-02-28 13:40:41 -0600

    Added iFrame tags to each exercise

--- client/src/pages/hip.js
+++ client/src/pages/hip.js
@@ -4,8 +4,28 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 
-function Myofascial() {
-    return 
-};
+function Hip() {
+    return (
+      <div>
+        <Hero>
+            <h1>Hip</h1>
+        </Hero>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
 
-export default hip;
+<p>
+<iframe src="https://www.instagram.com/p/BpDhNwEAiGh/embed/captioned" width="640" height="745" frameborder="0" scrolling="no" allowtransparency="true"></iframe>;
+
+<iframe src="hhttps://www.instagram.com/p/BzqK8Alj0aN/embed/captioned" width="640" height="745" frameborder="0" scrolling="no" allowtransparency="true"></iframe>;
+
+<iframe src="https://www.instagram.com/p/Bz9ZgrRFKxV/embed/captioned" width="640" height="745" frameborder="0" scrolling="no" allowtransparency="true"></iframe>;
+
+<iframe src="https://www.instagram.com/p/Bz_Gwm6jX3V/embed/captioned" width="640" height="745" frameborder="0" scrolling="no" allowtransparency="true"></iframe>;
+</p>
+              </Col>
+          </Row>
+       </Container>
+    </div>
+  );
+}
--- client/src/pages/shoulder.js
+++ client/src/pages/shoulder.js
@@ -5,7 +5,27 @@ import Row from "../components/Row";
 import Col from "../components/Col";
 
 function Shoulder() {
-    return 
-};
+    return (
+      <div>
+        <Hero>
+            <h1>Hip</h1>
+        </Hero>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
 
-export default shoulder;
+<p>
+<iframe src="https://www.instagram.com/p/BrpujmWl5iW/embed/captioned" width="640" height="745" frameborder="0" scrolling="no" allowtransparency="true"></iframe>;
+
+<iframe src="https://www.instagram.com/p/B2mC2C2DPiM/embed/captioned" width="640" height="745" frameborder="0" scrolling="no" allowtransparency="true"></iframe>;
+
+<iframe src="https://www.instagram.com/tv/B3kANWyAi5I/embed/captioned" width="640" height="745" frameborder="0" scrolling="no" allowtransparency="true"></iframe>;
+
+<iframe src="https://www.instagram.com/tv/B33bfcZi3Lc/embed/captioned" width="640" height="745" frameborder="0" scrolling="no" allowtransparency="true"></iframe>;
+</p>
+              </Col>
+          </Row>
+       </Container>
+    </div>
+  );
+}
