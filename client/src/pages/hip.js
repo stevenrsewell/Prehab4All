bdf2eab8d74b70dd3a8a6a080ecc8e97476464e0
Steven Sewell <stevenrsewell@outlook.com>
2020-02-28 12:12:35 -0600

    cervical and hip pages

--- client/src/pages/hip.js
+++ client/src/pages/hip.js
@@ -1,3 +1,26 @@
-import React from "react";
+// import React from "react";
+// import Hero from "../components/Hero";
+// import Container from "../components/Container";
+// import Row from "../components/Row";
+// import Col from "../components/Col";
 
-export default hip;
+// function About() {
+//   return (
+//     <div>
+//       <Hero>
+//         <h1>Prehab: See you in health!</h1>
+//         <h2>Hip</h2>
+//       </Hero>
+      
+//       <Container style={{ marginTop: 30 }}>
+//         <Row>
+//           <Col size="md-12">
+
+//           </Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default hip;
