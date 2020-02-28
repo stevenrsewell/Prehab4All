e6c3437788a5b30f33fd1cb912d0fae82b248968
Steven Sewell <stevenrsewell@outlook.com>
2020-02-28 12:24:19 -0600

    pages

--- client/src/pages/hip.js
+++ client/src/pages/hip.js
@@ -1,26 +1,11 @@
-// import React from "react";
-// import Hero from "../components/Hero";
-// import Container from "../components/Container";
-// import Row from "../components/Row";
-// import Col from "../components/Col";
+import React from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
 
-// function About() {
-//   return (
-//     <div>
-//       <Hero>
-//         <h1>Prehab: See you in health!</h1>
-//         <h2>Hip</h2>
-//       </Hero>
-      
-//       <Container style={{ marginTop: 30 }}>
-//         <Row>
-//           <Col size="md-12">
+function Myofascial() {
+    return 
+};
 
-//           </Col>
-//         </Row>
-//       </Container>
-//     </div>
-//   );
-// }
-
-// export default hip;
+export default hip;
