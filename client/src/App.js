c25db62365eb4ed9829146880130693644ba09de
Brad Bryant <bradleybryant86@gmail.com>
2020-02-28 11:17:21 -0600

    yep

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
 // import React, { Component } from "react";
 import React from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
-// import { BrowserRouter as Router} from "react-router-dom";
-import Directory from "./pages/Directory";
-// import About from "./pages/Signin";
-// import About from "./pages/Login";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
-
-import React from 'react';
-// import { Router ,Switch ,Route } from "react-router-dom";
 import Login from "./components/Login"
-
 import Signup from "./components/Signup"
 function App() {
 
