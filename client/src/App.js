18f96ebeb26033d19e609451b2e466347a6e8838
Steven Sewell <stevenrsewell@outlook.com>
2020-03-02 14:51:37 -0600

    Revert "Brad"

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Myofascial from "./pages/Myofascial";
 import Cervical from "./pages/Cervical";
 import Login from "./pages/Login"
 import Choices from "./pages/Choices"
+import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
 
